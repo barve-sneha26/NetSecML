19c25241e6a549f376641660b22c0a0f78828c9e
barve-sneha26 <barve.sneha26@outlook.com>
2024-11-10 15:21:28 -0500

    Model Training evaluation and MLFLow

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -15,6 +15,9 @@ TEST_FILE_NAME: str = "test.csv"
 
 SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml") 
 
+SAVED_MODEL_DIR = os.path.join("saved_models") 
+MODEL_FILE_NAME = "model.pkl" 
+
 # Data Ingestion related constants 
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData" 
 DATA_INGESTION_DATABASE_NAME: str = "NETWORK_DB" 
@@ -22,6 +25,7 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store" 
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
 
 #Data Validation related constant 
 DATA_VALIDATION_DIR_NAME: str = "data_validation" 
@@ -31,6 +35,8 @@ DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 
 PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
+
 
 # Data Validation related constants
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
@@ -43,3 +49,16 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "weights": "uniform",
 }
 
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy" 
+
+
+
+#  Model Trainer ralated constants 
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
+
+# TRAINING_BUCKET_NAME = "netwworksecurity"
